66a5f3e6e1ddad5495b3619d351abe1b93219423
DataMonarch <toghrultahirov2@gmail.com>
2022-01-03 16:22:51 +0400

    Added introduction

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 print('Hello World!')
 print('I love this project')
+
+def introduce():
+    name = input("What's your name?")
+    print("Fuck you, %s!" % name)
+
+if __name__ == '__main__':
+    introduce()
